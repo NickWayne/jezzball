95a28d328120bc8ad81a02f18cedc1b99b539327
Nick Wayne <wayne.nicholas16@gmail.com>
2017-10-19 17:59:56 -0400

    added cpp source

---
+++ src/funcs.cpp
@@ -0,0 +1,19 @@
+#include "funcs.h"
+#include <math.h>
+#include <allegro.h>
+#include <stdlib.h>
+
+double Tcos( int ang ){
+	return cos( ang * AL_PI / 180.0 );
+}
+
+double Tsine( int ang ){
+	return sin( ang * AL_PI / 180.0 );
+}
+
+int rnd( int q ){
+
+	if ( q <= 0 ) return 0;
+	return rand() % q;
+
+}
